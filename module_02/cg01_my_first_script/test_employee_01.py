0dd30aafd346768c31c886e9e1512cd190827dec
Mark Peters <mapeters101@earthlink.net>
2024-10-21 17:46:47 -0400

    Lesson29: Work on writing basic tests (Employee Module) - Set code per start of lecture.

---
+++ module_02/cg01_my_first_script/test_employee_01.py
@@ -0,0 +1,13 @@
+# TODO: Import Employee Class
+import pytest
+from module_02.cg00_apps.employee import Employee
+
+# TODO: Write a test for the method "base_annual_salary"
+#def test_base_annual_salary():
+
+
+# TODO: base_annual_salary" less than 45k
+#def test_base_annual_salary_less_than_45k():
+
+# TODO: base_annual_salary" greater than 120k
+#def test_base_annual_salary_greater_than_120k():
