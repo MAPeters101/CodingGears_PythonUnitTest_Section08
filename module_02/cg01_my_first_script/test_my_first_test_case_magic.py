97d9b72ede4e6d20ed07ba974cb553fe3e4c30bd
Mark Peters <mapeters101@earthlink.net>
2024-10-21 16:54:55 -0400

    Lesson27: Work on My first Unit Test with pytest up to 5:00.

--- module_02/cg01_my_first_script/test_my_first_test_case_magic.py
+++ module_02/cg01_my_first_script/test_my_first_test_case_magic.py
@@ -1,6 +1,10 @@
 # TODO: Import Magic Class
+import module_02.cg00_apps.magic as m
 
 # TODO: Write a test for the method "get_a_number"
+def test_get_a_number():
+    result = m.get_a_number()
+    assert result == 25
 
 # TODO: Write a test for the method "get_double"
 
