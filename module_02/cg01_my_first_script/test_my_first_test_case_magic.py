82611620a3d75e6cb516e59416b371f29c5cd280
Mark Peters <mapeters101@earthlink.net>
2024-10-21 17:35:40 -0400

    Lesson27: Complete work on My first Unit Test with pytest from 5:00.

--- module_02/cg01_my_first_script/test_my_first_test_case_magic.py
+++ module_02/cg01_my_first_script/test_my_first_test_case_magic.py
@@ -6,6 +6,15 @@ def test_get_a_number():
     result = m.get_a_number()
     assert result == 25
 
+
 # TODO: Write a test for the method "get_double"
+def test_get_double():
+    result = m.get_double(50)
+    assert result == 100
+
 
 # TODO: Write a test for the method "get_a_list"
+def test_get_a_list():
+    result = m.get_a_list('a', 'b', 'c', 'd')
+    assert result == ['a', 'b', 'c', 'd']
+
