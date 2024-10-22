ca0ea9d4fe269048b5bac0d8303a12cff8627195
Mark Peters <mapeters101@earthlink.net>
2024-10-22 11:54:11 -0400

    Lesson34: Complete work on Test Fixture.

--- module_02/cg06_test_fixtures/test_fixtures_01_calculator_01.py
+++ module_02/cg06_test_fixtures/test_fixtures_01_calculator_01.py
@@ -5,11 +5,24 @@ from module_02.cg00_apps.calculator_v2 import Calculator
 
 
 def test_addition():
-    pass
+    calci = Calculator()
+    calci.number1 = 10
+    calci.number2 = 50
+    result = calci.calc_add()
+    assert result == 60
+
 
 def test_difference():
-    pass
+    calci = Calculator()
+    calci.number1 = 10
+    calci.number2 = 50
+    result = calci.calc_diff()
+    assert result == -40
 
 
 def test_multiply():
-    pass
+    calci = Calculator()
+    calci.number1 = 10
+    calci.number2 = 50
+    result = calci.calc_prod()
+    assert result == 500
