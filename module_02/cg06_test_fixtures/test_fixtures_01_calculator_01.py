e3bcce4e82e91e162a38d832160e54816889688a
Mark Peters <mapeters101@earthlink.net>
2024-10-22 11:44:09 -0400

    Lesson34: Work on Test Fixture.  Initial setup from author.

---
+++ module_02/cg06_test_fixtures/test_fixtures_01_calculator_01.py
@@ -0,0 +1,15 @@
+# Topic: Test Fixtures
+
+import pytest
+from module_02.cg00_apps.calculator_v2 import Calculator
+
+
+def test_addition():
+    pass
+
+def test_difference():
+    pass
+
+
+def test_multiply():
+    pass
---
+++ module_02/cg06_test_fixtures/test_fixtures_02_calculator_02_with_fixture.py
@@ -0,0 +1,16 @@
+# Topic: Test Fixtures
+
+import pytest
+from module_02.cg00_apps.calculator_v2 import Calculator
+
+
+def test_addition(create_test_env):
+    pass
+
+
+def test_difference(create_test_env):
+    pass
+
+
+def test_multiply(create_test_env):
+    pass
