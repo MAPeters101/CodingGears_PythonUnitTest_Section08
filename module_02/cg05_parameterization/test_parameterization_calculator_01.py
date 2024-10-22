e22e2c53700ff99aeb47bffeabfbf40afaa504fc
Mark Peters <mapeters101@earthlink.net>
2024-10-22 11:39:20 -0400

    Lesson33: Complete work on Parameterize.

--- module_02/cg05_parameterization/test_parameterization_calculator_01.py
+++ module_02/cg05_parameterization/test_parameterization_calculator_01.py
@@ -2,7 +2,8 @@ import pytest
 from module_02.cg00_apps.calculator import Calculator
 
 
-def test_difference():
-    calci = Calculator(50, 20)
+@pytest.mark.parametrize("num1, num2, exp_result", [(50, 20, 30),(30, 20, 10), (-50, 20, -70)])
+def test_difference(num1, num2, exp_result):
+    calci = Calculator(num1, num2)
     result = calci.calc_diff()
-    assert result == 30
+    assert result == exp_result
